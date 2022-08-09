a039fed9909028fff5d263ad78f379db87ecf4de
Alexandra-Vargas <alexaholalexa@gmail.com>
2022-08-09 11:01:31 -0500

    cifrado cesar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,43 @@
 const cipher = {
-  // ...
-};
+  encode: function (offset, mensajeIngresado) {
+    //string
+    //recibimos parametros de la funcion
+    // console.log(offset, mensajeIngresado)
+    let resultado = "";
+    //encode: function (offset, mensajeIngresado) {
+    offset = parseInt(offset);
+    let msnMayus = mensajeIngresado.toUpperCase();
 
+    for (let i = 0; i < msnMayus.length; i++) {
+      //inicializador parte de 0 <condicion.largo; inccrementando a la derecha
+      let txtAscci = msnMayus.charCodeAt(i);
+      let newAscci = ((txtAscci - 65 + offset) % 26) + 65;
+
+      resultado += String.fromCharCode(newAscci); //string: objeto/clase. concatenar y guardar
+    }
+    return resultado;
+  },
+
+  decode: function (offset, mensajeIngresado) {
+    //string
+    //recibimos parametros de la funcion
+    // console.log(offset, mensajeIngresado)
+    let resultado = "";
+    //encode: function (offset, mensajeIngresado) {
+    offset = parseInt(offset);
+    let msnMayus = mensajeIngresado.toUpperCase();
+    //console.log('tamaño:', msnMayus.length) //lenght: obtiene la cantidad de caracteres
+
+    for (let i = 0; i < msnMayus.length; i++) {
+      //inicializador parte de 0 <condicion.largo; inccrementando a la derecha
+      //console.log(msnMayus[i]); //obtener el caracter dentro de la posicion de un [array]
+      let txtAscci = msnMayus.charCodeAt(i);
+      let newAscci = ((txtAscci + 65 - offset) % 26) + 65;
+      console.log(txtAscci, newAscci);
+
+      resultado += String.fromCharCode(newAscci); //string: objeto/clase. concatenar y guardar
+    }
+    return resultado;
+  },
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 console.log(cipher);
+
+const btnEncode = document.getElementById("btnEncode");
+btnEncode.addEventListener("click", btnCifrar);
+function btnCifrar() {
+  let textEncode = document.getElementById("mensajeIngresado").value;
+  let textOffset = document.getElementById("n_offset_d").value;
+  let resultado = document.getElementById("resultado");
+  resultado.value = cipher.encode(textOffset, textEncode); //mandamos argumentos a la funcion encode
+}
+
+const btnDecode = document.getElementById("btn_noCodificar");
+btnDecode.addEventListener("click", btnDecifrar);
+
+function btnDecifrar() {
+  let textDecode = document.getElementById("mensajeIngresado").value;
+  let textOffset = document.getElementById("n_offset_d").value;
+  let resultado = document.getElementById("resultado");
+  resultado.value = cipher.decode(textOffset, textDecode);
+}
